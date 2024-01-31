31456279435ed7a8409a356294f745afd4ad64c6
Vakhtangi Papidze <vakhtangi@layerise.com>
2024-01-31 09:36:32 +0400

    moved types into separate file

---
+++ src/types/types.ts
@@ -0,0 +1,71 @@
+import { IconName, IconPrefix } from "@fortawesome/fontawesome-svg-core";
+
+export interface ISkill {
+  icon?: [IconPrefix, IconName];
+  value: string,
+};
+
+export interface IEducation {
+  startDate: string;
+  endDate: string;
+  university: string;
+  degree: string;
+  field: string;
+};
+
+export interface IJobProject {
+  title: string;
+  description?: string;
+  responsibilities: string[];
+  url?: string;
+};
+
+export interface IJob {
+  startDate: string;
+  endDate: string;
+  company: string;
+  title: string;
+  location: 'onsite' | 'remote' | 'hybrid',
+  projects: IJobProject[];
+  technologies: ISkill[];
+}
+
+export interface ISkillOld {
+  iconPrefix: string,
+  icon: string,
+  value: string,
+};
+
+export interface IPersonalDetail extends ISkillOld {
+  url?: string;
+}
+
+export interface ITrainings {
+  title: string;
+  iconPrefix: string;
+  icon: string;
+  certified: boolean;
+}
+
+export interface IDataProvider {
+  personalInfo: {
+    profileImage: string;
+    fullName: string;
+    titles: string[];
+    details: IPersonalDetail[];
+    skills: {
+      softSkills: string[];
+      cloudSkills: ISkillOld[];
+      frontEndSkills: ISkillOld[];
+      backEndSkills: ISkillOld[];
+      devOpsSkills: ISkillOld[];
+      tools: ISkillOld[];
+      os: ISkillOld[];
+      languages: string[];
+    }
+  },
+  trainings: ITrainings[];
+  summary: string;
+  eductaion: IEducation[];
+  jobs: IJob[];
+}
